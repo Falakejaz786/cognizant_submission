4a3804c5780f9660cdbab89edeede9ad3e187a57
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 13:34:25 +0530

    Create Exercise11_DependencyInjection.java

---
+++ Engineering concepts/Design Patterns and Principles/Exercise11_DependencyInjection.java
@@ -0,0 +1,44 @@
+interface CustomerRepository {
+
+    String findCustomerById(int id);
+}
+
+class CustomerRepositoryImpl
+implements CustomerRepository {
+
+    public String findCustomerById(int id) {
+        return "Customer " + id;
+    }
+}
+
+class CustomerService {
+
+    private CustomerRepository repo;
+
+    CustomerService(
+            CustomerRepository repo) {
+
+        this.repo = repo;
+    }
+
+    void getCustomer(int id) {
+
+        System.out.println(
+            repo.findCustomerById(id)
+        );
+    }
+}
+
+public class Exercise11_DependencyInjection {
+
+    public static void main(String[] args) {
+
+        CustomerRepository repo =
+            new CustomerRepositoryImpl();
+
+        CustomerService service =
+            new CustomerService(repo);
+
+        service.getCustomer(1);
+    }
+}
