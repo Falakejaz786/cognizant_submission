29afdb69dd4d40d58784780cce9eb41174ff6ae1
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 13:00:04 +0530

    Create student-profile.component.ts

---
+++ Angular_handson_FalakEjaz/handson6/student-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { EnrollmentService } from '../../services/enrollment.service';
+
+@Component({
+  selector: 'app-student-profile',
+  standalone: true,
+  templateUrl: './student-profile.component.html'
+})
+export class StudentProfileComponent {
+
+  constructor(
+    public enrollmentService: EnrollmentService
+  ) {}
+
+}
