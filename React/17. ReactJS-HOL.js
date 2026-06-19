41e1b156468890a63c91754cc2f5f173e4a494f3
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 15:28:24 +0530

    Create 17. ReactJS-HOL.js

---
+++ React/17. ReactJS-HOL.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Getuser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      loading: true,
+    };
+  }
+
+  // Lifecycle method
+  async componentDidMount() {
+    try {
+      const response = await fetch("https://api.randomuser.me/");
+      const data = await response.json();
+
+      this.setState({
+        user: data.results[0],
+        loading: false,
+      });
+    } catch (error) {
+      console.log("Error fetching user:", error);
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    const { user, loading } = this.state;
+
+    if (loading) {
+      return <h2>Loading user...</h2>;
+    }
+
+    return (
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h2>User Details</h2>
+
+        <img
+          src={user.picture.large}
+          alt="user"
+          style={{ borderRadius: "50%" }}
+        />
+
+        <h3>
+          {user.name.title} {user.name.first}
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Getuser;
