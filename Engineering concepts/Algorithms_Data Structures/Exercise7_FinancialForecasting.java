36ccb37a5d96791c8d449197097c5e39de735a25
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 13:21:09 +0530

    Create Exercise7_FinancialForecasting.java

---
+++ Engineering concepts/Algorithms_Data Structures/Exercise7_FinancialForecasting.java
@@ -0,0 +1,17 @@
+public class Exercise7_FinancialForecasting {
+
+    static double futureValue(double amount, double rate, int years) {
+
+        if (years == 0)
+            return amount;
+
+        return futureValue(amount * (1 + rate), rate, years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+            futureValue(1000, 0.1, 3)
+        );
+    }
+}
