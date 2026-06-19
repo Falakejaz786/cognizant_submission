f73d2918b78162244e00b41b3fea35e5cc557860
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 12:39:45 +0530

    Create home.component.ts

---
+++ Angular_handson_FalakEjaz/handson2/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+}
