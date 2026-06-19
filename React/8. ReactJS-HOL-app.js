0f93e4dbee728ba07617e9c8fc87d086fde199a7
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 15:18:14 +0530

    Create 8. ReactJS-HOL-app.js

---
+++ React/8. ReactJS-HOL-app.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class CountPeople extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      entryCount: 0,
+      exitCount: 0
+    };
+  }
+
+  UpdateEntry = () => {
+    this.setState({
+      entryCount: this.state.entryCount + 1
+    });
+  };
+
+  UpdateExit = () => {
+    this.setState({
+      exitCount: this.state.exitCount + 1
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Mall Counter Application</h1>
+
+        <h2>People Entered: {this.state.entryCount}</h2>
+        <h2>People Exited: {this.state.exitCount}</h2>
+
+        <button onClick={this.UpdateEntry}>Login</button>
+        <button
+          onClick={this.UpdateExit}
+          style={{ marginLeft: "10px" }}
+        >
+          Exit
+        </button>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return <CountPeople />;
+}
+
+export default App;
