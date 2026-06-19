dc937e7ad78430616dd6d443c221277ca6f3b60b
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 15:17:14 +0530

    Create 7. ReactJS-HOL-app.js

---
+++ React/7. ReactJS-HOL-app.js
@@ -0,0 +1,31 @@
+Objectives
+
+Define Props
+Explain Default Props
+Identify the differences between State and Props
+Explain reactDOM.render()
+
+In this hands-on lab, you will learn how to:
+Use Props
+Apply reactDOM.render()
+Prerequisites
+
+The following is required to complete this hands-on lab:
+Node.js
+NPM
+Visual Studio Code
+
+Notes
+
+Estimated time to complete this lab: 60 minutes.
+Create a React Application named “shoppingapp” with a class component named “OnlineShopping” and “Cart”.
+1.In Cart class, create 2 properties as mentioned below:
+Itemname
+Price
+
+2.In OnlineShopping class, create an array of Cart and initialize 5 items.
+
+
+
+3.Loop through these items and display the data as shown below:
+
