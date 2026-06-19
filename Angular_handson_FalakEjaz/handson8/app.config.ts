3300b1baac060c54b7c6f07f81d15118887faa69
Falak Ejaz <157350415+Falakejaz786@users.noreply.github.com>
2026-06-19 13:11:39 +0530

    Add HTTP client with interceptor to app config

--- Angular_handson_FalakEjaz/handson8/app.config.ts
+++ Angular_handson_FalakEjaz/handson8/app.config.ts
@@ -3,3 +3,21 @@ import { provideHttpClient } from '@angular/common/http';
 providers: [
   provideHttpClient()
 ]
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
+import {
+  authInterceptor
+} from './interceptors/auth.interceptor';
+
+providers: [
+
+  provideHttpClient(
+    withInterceptors([
+      authInterceptor
+    ])
+  )
+
+]
